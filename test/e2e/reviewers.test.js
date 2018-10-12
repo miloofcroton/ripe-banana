f9933dbf40c8be071b223110eb777ed9a3679998
Davidchhing <davidhchhing@gmail.com>
2018-10-12 11:34:59 -0700

    made reviewer get all and create test

---
+++ test/e2e/reviewers.test.js
@@ -0,0 +1,69 @@
+const { dropCollection } = require('../util/db');
+const request = require('supertest');
+const app = require('../../lib/app');
+const Chance = require('chance');
+const chance = new Chance();
+
+describe('end to end reviewer testing', () => {
+
+    let reviewers = [
+        {
+            name: chance.name(),
+            company: chance.company()
+        },
+        {
+            name: chance.name(),
+            company: chance.company()
+        },
+        {
+            name: chance.name(),
+            company: chance.company()
+        },
+    ];
+
+    let createdReviewers;
+
+    const tumblr = reviewer => {
+        return request(app)
+            .post('/reviewer')
+            .send(reviewer)
+            .then(res => res.body);
+    };
+
+    beforeEach(() => {
+        return dropCollection('reviewers');
+    });
+
+    beforeEach(() => {
+        return Promise.all(reviewers.map(tumblr))
+            .then(reviewerRes => createdReviewers = reviewerRes);
+    });    
+    
+    it('this creates a reviewer', () => {
+        const reviewer = {
+            name: chance.name(),
+            company: chance.company()
+        };
+        return request(app)
+            .post('/reviewer')
+            .send(reviewer)
+            .then(({ body }) => {
+                expect(body).toEqual({
+                    ...reviewer,
+                    _id: expect.any(String),
+                    __v: expect.any(Number)
+                });
+            });
+    });
+
+    it('gets all reviewers', () => {
+        return request(app)
+            .get('/reviewer')
+            .then(retrievedReviewers => {
+                createdReviewers.forEach(createdReviewer => {
+                    expect(retrievedReviewers.body).toContainEqual({ _id: createdReviewer._id, name: createdReviewer.name, company: createdReviewer.company });
+                });
+            });
+    });
+    
+});
