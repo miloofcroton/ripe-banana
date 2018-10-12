ad2b32cd597a77534932f8dbe1192dea33b9d1ef
Davidchhing <davidhchhing@gmail.com>
2018-10-12 09:08:01 -0700

    Actor models tested and passing

---
+++ test/unit/actor.test.js
@@ -0,0 +1,36 @@
+const { getErrors } = require('../util/helpers');
+const Actor = require('../../lib/models/Actor');
+const Chance = require('chance');
+const chance = new Chance();
+
+describe('Actor model', () => {
+
+    it('validates a good model', () => {
+
+        const data = {
+            name: chance.name(),
+            dob: chance.birthday(),
+            pob: chance.city()
+        };
+
+        const actor = new Actor(data);
+        const jsonActor = actor.toJSON();
+        expect(jsonActor).toEqual({ ...data, _id: expect.any(Object) });
+    });
+
+    it('requires an event type', () => {
+
+        const actor = new Actor({
+            dob: chance.birthday(),
+            pob: chance.city()
+        });
+
+        const errors = getErrors(actor.validateSync(), 1);
+        expect(errors.name.properties.message).toEqual('Path `name` is required.');
+    });
+
+
+
+
+
+});
